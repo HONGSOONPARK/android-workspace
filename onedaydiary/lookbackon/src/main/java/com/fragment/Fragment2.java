5c7c23cbd56f73621fac5a630673f045a6ed7383
parkhongsoon <kongsooni39@gmail.com>
2020-02-18 21:19:05 +0900

    add fragment
    add fragment

---
+++ onedaydiary/lookbackon/src/main/java/com/fragment/Fragment2.java
@@ -0,0 +1,31 @@
+package com.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.slow.diary.R;
+
+public class Fragment2 extends Fragment {
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
+
+
+        initUI(rootView);
+
+        return rootView;
+    }
+
+
+    private void initUI(ViewGroup rootView){
+    }
+
+}
---
+++ onedaydiary/lookbackon/src/main/java/com/service/OnTabItemSelectedListener.java
@@ -0,0 +1,7 @@
+package com.service;
+
+public interface OnTabItemSelectedListener {
+
+    public void onTabSelected(int position);
+
+}
