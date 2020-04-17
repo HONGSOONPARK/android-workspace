659c6fa71e6b6627df676daf4d6a47deee8f50d2
parkhongsoon <phs39@emoney.co.kr>
2020-04-17 17:17:40 +0900

    std2
    std2

---
+++ kotlinstd2/app/src/main/java/com/emoney/kotlinstd2/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.emoney.kotlinstd2
+
+
+import android.os.Bundle
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewParent
+
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    var view_list = ArrayList<View>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        view_list.add(layoutInflater.inflate(R.layout.view1, null))
+        view_list.add(layoutInflater.inflate(R.layout.view2, null))
+        view_list.add(layoutInflater.inflate(R.layout.view3, null))
+
+        pager.adapter = CustomAdapter()
+
+
+                pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+
+                    override fun onPageScrollStateChanged(p0: Int) {
+
+                    }
+
+                    override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
+                        textView.text = "${p0} 번째 뷰가 나타났습니다"
+
+            }
+
+            override fun onPageSelected(p0: Int) {
+
+            }
+        })
+
+
+
+
+    }
+
+
+    inner class CustomAdapter : PagerAdapter() {
+        override fun getCount(): Int {
+            return view_list.size
+        }
+
+        override fun isViewFromObject(view: View, `object`: Any): Boolean {
+            return view == `object`
+        }
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+
+            pager.addView(view_list[position])
+
+            return view_list[position]
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            pager.removeView(`object` as View)
+        }
+    }
+}
