e753e97f18620e894b29007536a5cf1eb8c62459
parkhongsoon <kongsooni39@gmail.com>
2020-04-23 00:18:06 +0900

    list
    list

--- kotlinstd2/app/src/main/java/com/emoney/kotlinstd2/MainActivity.kt
+++ kotlinstd2/app/src/main/java/com/emoney/kotlinstd2/MainActivity.kt
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewParent
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,19 +22,37 @@ import kotlinx.android.synthetic.main.activity_main.textView as textView1
 class MainActivity : AppCompatActivity() {
     var view_list = ArrayList<View>()
 
+    var data = arrayOf("리스트1","리스트2","리스트3","리스트4","리스트5","리스트6","리스트7","리스트8")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_main)
 
         var listener1 = BtnListner()
 
-        button1.setOnClickListener(listener1);
+//        button1.setOnClickListener(btnListener);
+//        button2.setOnClickListener(btnListener);
 
-        button2.setOnClickListener(btnListener);
+        var main_adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
+        main_listView.adapter = main_adapter
 
+//        var listener = ListListener()
+//        main_listView.setOnItemClickListener(listener)
 
+        main_listView.setOnItemClickListener { parent, view, position, id ->
+            textView.text = data[position]
+
+        }
     }
+
+    inner class ListListener: AdapterView.OnItemClickListener{
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            textView.text = data[position]
+        }
+    }
+
 
     inner class BtnListner:View.OnClickListener{
         override fun onClick(v: View?) {
@@ -43,14 +63,15 @@ class MainActivity : AppCompatActivity() {
     var btnListener = View.OnClickListener { view ->
 
         when(view.id){
-            R.id.button1 -> {
-                textView.text = "첫번재 버튼"
-            }
-
-            R.id.button2 -> {
-                textView.text = "button2 버튼"
-            }
-
+//            R.id.button1 -> {
+//                textView.text = "첫번재 버튼"
+//
+//
+//            }
+//
+//            R.id.button2 -> {
+//                textView.text = "두번재 버튼"
+//            }
         }
 
 
